d97f5425998463e509496950d8c50d2353f04abf
Safa002 <sefaceferova@gmail.com>
2022-03-14 15:03:43 +0400

    Safa002

---
+++ main.js
@@ -0,0 +1,13 @@
+let number = prompt('Reqem daxil edin:');
+let change = number.split(' ')
+let num1 = change[0];
+let num2 = change[1];
+let a ;
+a = num1;
+
+num1 = num2;
+
+num2 = a;
+
+
+alert(num1 + ' ' + num2);
