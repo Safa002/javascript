3e96767be21208de39c74d1da917303e7ce3708e
Safa002 <sefaceferova@gmail.com>
2022-03-15 14:33:21 +0400

    Safa002

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//teacher's exercise
 let number = prompt('Reqem daxil edin:');
 let change = number.split(' ')
 let num1 = change[0];
@@ -11,3 +12,15 @@ num2 = a;
 
 
 alert(num1 + ' ' + num2);
+
+// Lesson-5 question-1
+let price = prompt('999.99 formatinda qiymet daxil edin:');
+let changee = price.split('.');
+let manattype = changee[0];
+let qepiktype = changee[1];
+alert(manattype + ' manat ' + qepiktype + ' qepik');
+ 
+//Lessin-5 question-2
+let salary = prompt('Emek haqqinizi daxil edin:');
+let decreased = (Number(salary)/13)*100;
+alert(decreased);
